a32e402f31cee98e0edf018223efc794cc71b869
Murshid V P <murshid.vp@kgisl.com>
2024-08-26 17:13:40 +0530

    adding all files

---
+++ main.py
@@ -0,0 +1,35 @@
+from fastapi import FastAPI,Request
+from models import Base
+from database import engine
+from routers import auth, todos, admin, users
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+import os
+
+app = FastAPI()
+
+Base.metadata.create_all(bind=engine)
+
+templates = Jinja2Templates(directory='templates')
+
+app.mount('/static',StaticFiles(directory="static"),name ='static')
+
+
+@app.get("/")
+def home_page(request:Request):
+    return templates.TemplateResponse("home.html",{'request':request})
+
+
+
+
+@app.get("/healthy")
+def health_check():
+    return {'status': 'Healthy'}
+
+
+
+
+app.include_router(auth.router)
+app.include_router(todos.router)
+app.include_router(admin.router)
+app.include_router(users.router)
